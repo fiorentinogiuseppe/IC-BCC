7f63431506d49b17ef17a7fb5b34aed23fc8213c
R3turnZ3r0 <netogiuseppe@hotmail.com>
2018-03-19 21:16:08 -0300

    DefaultDNN

--- Scripts/Giuseppe/script.py
+++ Scripts/Giuseppe/script.py
@@ -39,6 +39,94 @@ from glob import glob
 
 print(sys.version)
 
+# Retorna quantidade de imagens em diretorio
+def getNumSamples(src):
+    sum = 0
+    for cl in os.listdir(src):
+        class_dir = os.path.join(src, cl)
+        files = os.listdir(class_dir)
+        l = len(files)
+        sum += l
+
+    return sum, len(os.listdir(src))
+
+# Carrega base de dados e converte em array numpy
+def array_from_dir(data_dir, nb_samples, nb_classes, width, height):
+    images = np.zeros((nb_samples, width, height, 1))
+    y = np.zeros((nb_samples, nb_classes))
+
+    bar = progressbar.ProgressBar(max_value=len(os.listdir(data_dir)))
+
+    cont = 0
+    cl_index = 0
+    for cl in os.listdir(data_dir):
+        class_dir = os.path.join(data_dir, cl)
+        files = os.listdir(class_dir)
+        shuffle(files)
+        files_per_class = 0
+        for file in files:
+            files_per_class += 1
+
+            # lê em escala de cinza
+            img = cv2.imread(os.path.join(class_dir, file), flags=0)
+
+            # muda escala
+            img = cv2.resize(img, (width, height))
+
+            # deixa entre 0 e 1
+            img = np.array(img) / 255.0
+
+            # coloca no array
+            images[cont, :, :, 0] = img
+
+            # guarda a classe
+            y[cont] = to_categorical(cl_index, nb_classes)
+
+            cont += 1
+        cl_index += 1
+        bar.update(cl_index)
+
+    # retorna array com imagens em classes
+    return images, y
+
+
+# divide base aleatoriamente em treinamento, teste e validação
+def split_dataset_random(b_x, b_y, p_train, p_val):
+    total_samples = b_x.shape[0]
+    train_size = int(total_samples * p_train)
+    valid_size = int(total_samples * p_val)
+    test_size = total_samples - train_size - valid_size
+
+    print()
+    print('train set:', train_size, ' images')
+    print('validation set:', valid_size, ' images')
+    print('test set:', test_size, ' images')
+
+    x_tr = np.zeros((train_size, b_x[0].shape[0], b_x[0].shape[1], b_x[0].shape[2]))
+    y_tr = np.zeros((train_size, b_y[0].shape[0]))
+
+    x_val = np.zeros((valid_size, b_x[0].shape[0], b_x[0].shape[1], b_x[0].shape[2]))
+    y_val = np.zeros((valid_size, b_y[0].shape[0]))
+
+    x_te = np.zeros((test_size, b_x[0].shape[0], b_x[0].shape[1], b_x[0].shape[2]))
+    y_te = np.zeros((test_size, b_y[0].shape[0]))
+
+    from random import shuffle
+    index = list(range(total_samples))
+    shuffle(index)
+
+    for i in range(total_samples):
+        if i < train_size:
+            x_tr[i] = b_x[index[i]]
+            y_tr[i] = b_y[index[i]]
+        elif i < (train_size + valid_size):
+            x_val[i - train_size] = b_x[index[i]]
+            y_val[i - train_size] = b_y[index[i]]
+        else:
+            x_te[i - train_size - valid_size] = b_x[index[i]]
+            y_te[i - train_size - valid_size] = b_y[index[i]]
+
+    return x_tr, y_tr, x_te, y_te, x_val, y_val
 
 def load_notmnist(path='./notMNIST_small',letters='ABCDEFGHIJ',
                   img_shape=(28,28),test_size=0.25,one_hot=False):
@@ -124,7 +212,7 @@ def getNumSamples(src):
 
 #Script
 class script(Problem):
-        def __init__(self, name, base_x, base_y):
+        def __init__(self, name, base_x, base_y, lencategories):
         		      #0	        1	    2		      3
                 encoding = [Integer(1,9), Integer(0,1), Integer(1,2), Integer(1,124),
                 	      #4		5	    6		      7
@@ -139,51 +227,41 @@ class script(Problem):
                             Integer(3,6)]
 
                 variables = len(encoding)
-                objectives = 2
+                objectives = 1
                 super(script, self).__init__(variables, objectives)
                 self.types[:] = encoding
                 self.class_name = name
                 self.base_x=base_x
                 self.base_y=base_y
+                self.nb_db_samples, self.num_classes = getNumSamples(pasta_base)
 
-                self.batch_size = 200#de quanto em quanto vai caminhar
-                self.num_classes = 3
+                self.batch_size = 28#de quanto em quanto vai caminhar
                 self.epochs = 50#repeticoes
                 self.solucoes={}#dicionario de solucoes
                 self.id = 0
-                self.X_train=None
+                self.lencategories=lencategories
 
-        def DefaulttDNN(self,lencategories):
-              print("Train Shape",self.X_train.shape[1:])
+        def DefaulttDNN(self,input_shape,lencategories):
               model = Sequential()
-              input1=random.randint(1,9)
-              input2=random.randint(1,124)
-              input3=random.randint(1,2)
-              input4=random.randint(1,2)
-              input5=random.randint(1,9)
-              input6=random.randint(1,124)
-              input7=random.randint(1,2)
-              model.add(Conv2D(filters=input1, kernel_size=input2, strides=input3 ,padding="same", input_shape=self.X_train.shape[1:]))
-              model.add(Conv2D(filters=input1, kernel_size=input2, strides=input3 ,padding="same"))
-              pool=random.randint(2,3)
-
-              model.add(MaxPooling2D(pool_size=(pool,pool), strides=input4))
-              model.add(Conv2D(filters=input5, kernel_size=input6,strides=input7, padding="same"))
-              model.add(Flatten())
-              model.add(Dense(units=lencategories, activation='softmax'))
+	      model.add(Conv2D(filters=5, kernel_size=4, input_shape=input_shape))
+	      model.add(Conv2D(filters=5, kernel_size=4))
+	      model.add(MaxPooling2D())
+	      model.add(Conv2D(filters=5, kernel_size=4))
+	      model.add(Dense(units=5))
               return model
 
-        def ModifieddDNN(self,numCamadasModf, model, solution, lencategories):
+        def ModifieddDNN(self,numCamadasModf, model, solution, lencategories, input_shape):
             print(solution)
             md=Sequential()
             for i in range(1,7,1):
                   if(i<numCamadasModf or i==5):
                         md.add(model.get_layer(None,i))
                   else:
-                        if(i==1): md.add(Conv2D(filters=solution.variables[0], kernel_size=solution.variables[3], strides=solution.variables[2], padding="same",input_shape=self.X_train.shape[1:]))
-                        elif(i==2 or i==4): md.add(Conv2D(filters=solution.variables[10], kernel_size=solution.variables[13], strides=solution.variables[12], padding="same"))
+                        if(i==1): md.add(Conv2D(filters=solution.variables[0], kernel_size=solution.variables[3], strides=solution.variables[2], padding="same",input_shape=input_shape))
+                        elif(i==2): md.add(Conv2D(filters=solution.variables[4], kernel_size=solution.variables[7], strides=solution.variables[6], padding="same"))
                         elif(i==3): md.add(MaxPooling2D(pool_size=(solution.variables[8],solution.variables[8]), strides=solution.variables[9]))
-                        elif(i==6): md.add(Dense(units=lencategories,activation='softmax'))
+                        elif(i==4): md.add(Conv2D(filters=solution.variables[10], kernel_size=solution.variables[13], strides=solution.variables[12], padding="same"))
+                        elif(i==6): md.add(Dense(units=self.lencategories,activation='softmax'))
             return md
         def getSolucaoFinal(self,info):
                 return self.solucoes.get(info)
@@ -193,17 +271,17 @@ class script(Problem):
 
                 print("Rescaling database")
 
-                X_train,X_test, y_train,y_test = train_test_split(self.base_x,self.base_y,test_size=0.2,random_state=0) 
+                X_train, X_test, y_train, y_test = train_test_split(self.base_x,self.base_y,test_size=0.2,random_state=0) 
                                 
                 print("Criando o modelo")
 
                 self.X_train=X_train
 
-                modelPadrao= self.DefaulttDNN(lencategories=10)#pensar num jeito de automatizar
+                modelPadrao= self.DefaulttDNN(input_shape=self.X_train.shape[1:],lencategories=5)#pensar num jeito de automatizar
 
                 print(modelPadrao.summary())
 
-                model=self.ModifieddDNN(numCamadasModf=solution.variables[15], model=modelPadrao,solution=solution, lencategories=10)
+                model=self.ModifieddDNN(numCamadasModf=solution.variables[15], model=modelPadrao,solution=solution, lencategories=5, input_shape=self.X_train.shape[1:])
 
                 print(model.summary())
 
@@ -237,8 +315,8 @@ class script(Problem):
                 #val_f1 = history.history['val_f1'][-1]
 
 		#metrics################################################################
-                #print('\n acc' , val_acc)
-                #predictions = model.predict(X_test, batch_size=self.batch_size)
+                print('\n acc' , val_acc)
+                predictions = model.predict(X_test, batch_size=self.batch_size)
 
                 #print(classification_report(y_test.argmax(axis=1),
                 #                    predictions.argmax(axis=1)))
@@ -251,30 +329,13 @@ class script(Problem):
                 #train_epochs = len(history.history['acc'])
             	##########################################################################
 
-        	train_epochs = len(history.history['acc'])
-
-        	start = time.time()
-
-        	# Testa
-        	test_scores = model.evaluate(X_test, y_test, verbose=0)
-
-		end = time.time()
-		test_ms = round((end - start) * 1000, 1)
-		print("Test time: ", test_ms, " ms")
-
-		test_acc = test_scores[1]
-		print('Test accuracy:', test_acc)
-
-        
                 from keras import backend as K
                 if K.backend() == 'tensorflow':
                     K.clear_session()
 
                 self.id += 1
 
-                #solution.objectives[:] = [-val_acc] # -val_precision ,-val_recall , -val_f1]como o objectives la em cima ta 1 nao precisa do tempo
-		# retorna os objetivos (acuracia e tempo
-        	solution.objectives[:] = [-val_acc, test_ms]
+                solution.objectives[:] = [-val_acc] # -val_precision ,-val_recall , -val_f1]como o objectives la em cima ta 1 nao precisa do tempo
 
                 variaveis=[]
                 print(solution.objectives)
@@ -290,22 +351,29 @@ class script(Problem):
 #MAIN
 
 if __name__ == '__main__':
-    '''
+    print("Carregando Base")
+    
     #Base Gestos
-    problem=MP.MProblem(name='Problem')
-    optimizer = NSGAII(problem, population_size=10)
-    num_generations = 1
-    # inicia o algoritmo
-    geracao=0
-    for i in range(num_generations):
-            # executa por uma geracao
-            geracao+=1
-            print("::::Geracao: ",geracao,"::::")
-            optimizer.run(1)
 
-    for solution in optimizer.result:
-            print(solution.objectives)
-   
+    # indica pasta da base de dados, uma pasta com imagens para cada classe
+    pasta_base = 'DB/Gestures/'
+
+    # tamanho do batch de treinamento
+    batch_size = 200
+
+    nb_db_samples, num_classes = getNumSamples(pasta_base)
+
+    print('Base tem ', nb_db_samples, ' imagens e ', num_classes, ' classes')
+
+    # tamanho da imagem
+    options_im_size = [16, 32, 48, 64]
+    im_sz=random.randint(0,3)
+    img_width, img_height = options_im_size[im_sz], options_im_size[im_sz]
+
+    print('Rescaling database to', img_width, 'x', img_height, ' pixels')
+    base_x, base_y = array_from_dir(data_dir=pasta_base, nb_samples=nb_db_samples,
+                                        nb_classes=num_classes, width=img_width, height=img_height)
+    '''
     #Base Mnist
     # input image dimensions
     img_rows, img_cols = 28, 28 #configuração manual a respeito da base
@@ -371,45 +439,42 @@ if __name__ == '__main__':
     # convert class vectors to binary class matrices
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
-    '''
+    
     print("Configuração da Base")
     # input image dimensions
     img_rows, img_cols = 28, 28 #configuração manual a respeito da base que sera carregada (tamanho de cada imagem)
 
     #output dimension
     num_classes=10 #configuração manual a respeito da base que sera carregada (quantas classes tem a base)
-
-    print("Carregando Base")
+    '''
+    #print("Carregando Base")
     #Load DB
-    x_train, y_train, x_test, y_test=load_notmnist() #Caso queira carregar bases que nao sao do keras o processo eh o mesmo apenas obtenha o dados de treino
+    #x_train, y_train, x_test, y_test=load_notmnist() #Caso queira carregar bases que nao sao do keras o processo eh o mesmo apenas obtenha o dados de treino
                                                             # e teste e envie para a configuração de base. A bases de Gestos do prof sergio ja ta configurada int nao
-                                                            #precisa ir pra proxima fase. So ir direto pro problema
-      
-      
-    print("Configurando Base")
-    x_train, y_train, x_test, y_test= configBase(x_train, y_train, x_test, y_test, img_rows, img_cols)
+                                                            #precisa ir pra proxima fase. So ir direto pro problema    
+    #print("Configurando Base")
+    #x_train, y_train, x_test, y_test= configBase(x_train, y_train, x_test, y_test, img_rows, img_cols)#Usar para outras bases
 
     print("Configurando Problem")
-    #problem2=script(name='Problem',base_x=problem.getbaseX(),base_y=problem.getbaseY()) #Usar qndo for testar a base de gestos
-    problem2=script(name='Problem',base_x=x_train,base_y=y_train) #Qualquer outra base usar esse
+    problem2=script(name='Problem',base_x=base_x, base_y=base_y, lencategories=num_classes) #Qualquer outra base usar esse
     
     print("Configurando Otimizador")
     optimizer2= SMPSO(problem2,
-                 swarm_size = 5,
+                 swarm_size = 30,
                  leader_size = 5,
                  generator = RandomGenerator(),
                  mutation_probability = 0.1,
                  mutation_perturbation = 0.5,
-                 max_iterations = 10,
+                 max_iterations = 100,
                  mutate = None)
 
     print("Rodando o codigo")
-    num_generations=5
-    geracao=0
-    for i in range(num_generations):
+    num_repet=10
+    repeticao=0
+    for i in range(num_repet):
             # executa por uma geracao
-            geracao+=1
-            print("::::Geracao: ",geracao,"::::")
+            repeticao+=1
+            print("::::Repeticao: ",repeticao,"::::")
             optimizer2.run(1)
 
     print(">>>>>>>>>>>>>RESULT<<<<<<<<<<<<<<<")
@@ -417,3 +482,4 @@ if __name__ == '__main__':
     print("Objective",val[0])
     resp=problem2.getSolucaoFinal(val[0])
     print("Config",resp)
+
